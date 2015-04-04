134ea98c9cce34d66757bd6b57db4963ef6553a7
JonCarlee <joncarlee@gmail.com>
2015-04-04 16:10:15 -0400

    BAD

--- PersonalWebsite/js/custom.js
+++ PersonalWebsite/js/custom.js
@@ -171,21 +171,4 @@ $(document).ready(function(){
    return false;
 });
 
-function findMax() {
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var num3 = document.getElementById("num3").value;
-    var num4 = document.getElementById("num4").value;
-    var num5 = document.getElementById("num5").value;
-    if (isNan(num1 + num2 + num3 + num4 + num5) == true) {
-        document.getElementById("highest").innerHTML = "Please fill out all of the number spaces.";
-    }
-    else {
-        document.getElementById("highest").innerHTML = Math.max(num1, num2, num3, num4, num5);
-    }
-}
 
-$(document).ready(function () {
-
-    $("doMax").click(findMax);
-});
