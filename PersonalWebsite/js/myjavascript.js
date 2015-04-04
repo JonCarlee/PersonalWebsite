687bce97dbd61d2f10584fbcc2ec2b665c6f8410
JonCarlee <joncarlee@gmail.com>
2015-04-04 16:10:18 -0400

    Revert "BAD"
    This reverts commit 134ea98c9cce34d66757bd6b57db4963ef6553a7.

--- PersonalWebsite/js/myjavascript.js
+++ PersonalWebsite/js/myjavascript.js
@@ -1,1 +1,21 @@
-﻿
+﻿window.onload = start();
+
+
+function start() {
+        doMax.addEventListener("click", findMax());
+
+}
+
+function findMax() {
+    var num1 = document.getElementById(num1);
+    var num2 = document.getElementById(num2);
+    var num3 = document.getElementById(num3);
+    var num4 = document.getElementById(num4);
+    var num5 = document.getElementById(num5);
+    if (num1 + num2 + num3 + num4 + num5 == NaN) {
+        document.getElementById("highest") = "Please fill out all of the number spaces.";
+    }
+    else {
+        document.getElementById("highest") = Math.max(num1, num2, num3, num4, num5);
+    }
+}
